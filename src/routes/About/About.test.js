74b0ab3d08e6daf686c37baf549155d77055edc7
Max Starr <manmon42@gmail.com>
2018-08-10 14:40:30 -0700

    Refactored tests for accurate coverage reports

--- src/routes/About/About.test.js
+++ src/routes/About/About.test.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { MemoryRouter } from 'react-router-dom';
 
 import About from './About';
 
 it('renders without crashing', () => {
-  shallow(
-    <MemoryRouter>
-      <About />
-    </MemoryRouter>
-  );
+  shallow(<About />);
 });
--- src/routes/Home/Home.test.js
+++ src/routes/Home/Home.test.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { MemoryRouter } from 'react-router-dom';
 
 import Home from './Home';
 
 it('renders without crashing', () => {
-  shallow(
-    <MemoryRouter>
-      <Home />
-    </MemoryRouter>);
+  shallow(<Home />);
 });
