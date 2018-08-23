57da9d6195a2dd40459300fad63060223c6ed3e2
Duncan Grubbs <duncan.grubbs@gmail.com>
2018-08-22 19:28:28 -0700

    Add copywrite, footer adjustments.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,24 +6,28 @@ class Footer extends Component {
   render() {
     return(
       <div id="footer">
-        <div className="footer-text-section">
-          <span className="footer-header">Contact</span>
-          <div className="footer-link">Pricing</div>
-          <div className="footer-link">Job Request</div>
-          <div className="footer-link">Feedback</div>
+        <div id="footer-links">
+          <div className="footer-text-section">
+            <span className="footer-header">Contact</span>
+            <div className="footer-link">Pricing</div>
+            <div className="footer-link">Job Request</div>
+            <div className="footer-link">Feedback</div>
+          </div>
+
+          <div className="footer-text-section">
+            <span className="footer-header">Company</span>
+            <div className="footer-link">Careers</div>
+            <div className="footer-link">Our Story</div>
+          </div>
+
+          <div className="footer-text-section">
+            <span className="footer-header">Legal</span>
+            <div className="footer-link">Privacy Policy</div>
+            <div className="footer-link">Terms of Service</div>
+          </div>
         </div>
 
-        <div className="footer-text-section">
-          <span className="footer-header">Company</span>
-          <div className="footer-link">Careers</div>
-          <div className="footer-link">Our Story</div>
-        </div>
-
-        <div className="footer-text-section">
-          <span className="footer-header">Legal</span>
-          <div className="footer-link">Privacy Policy</div>
-          <div className="footer-link">Terms of Service</div>
-        </div>
+        <div id="copywrite">© 2018 room127. All Rights Reserved.</div>
       </div>
     )
   }
